37d62bccf907e48b6496dfa0e3ac0d605de2455f
GustavoNM <dev@gruposeara.com>
2022-01-16 13:21:36 -0600

    add babel

--- src/index.js
+++ src/index.js
@@ -6,24 +6,26 @@ const elvenShield= {
 }
 
 const elvenShield1= {
-    A:90,
-    B:90,
-    C:90,
-    D:90
+    A1:90,
+    B1:90,
+    C1:90,
+    D1:90
 }
 
 const A = {
-    elvenShield1,
-    elvenShield,
-    Y:{ty:2,op:5}
+    ...elvenShield1,
+    ...elvenShield,
 };
 
-
+function rand(I){
+    return (I - Math.floor(Math.random()*10))
+}
 let DOM = document.getElementsByTagName('body')[0], i=5;
 setInterval(()=>{
+    let A = Math.floor()
     i = (i >= 255) ? 5 : i;
     i+=10;
-    DOM.style.backgroundColor =  `rgb(${i-2},${i-10},${i-9})`;
+    DOM.style.backgroundColor =  `rgb(${rand(i)},${rand(i)},${rand(i)})`;
 },500);
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
+let mode = "development";
+
+if(process.env.NODE_ENV === "production"){
+    mode = "production";
+};
+
 module.exports = {
-    mode:"development",
-    devTool: false,
+    mode,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    // devtool: false,
     devServer: {
         static: "./dist"
     }
